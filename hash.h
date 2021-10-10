5a5c1ba3514ec52a98cfd4ba58fc6402934d8e4d
Van Allan Park <vpark05@unix2.csc.calpoly.edu>
2021-10-10 14:31:47 -0700

    save

--- hash.h
+++ hash.h
@@ -4,3 +4,36 @@ Project: CPE 357 Project 2
 Author: Dylan Baxter, Van Park
 Description:
 */
+
+#include<string.h>
+#include<stdlib.h>
+#define ISIZE = 1024
+
+struct Node{
+    int freq
+    char *word;
+}
+
+struct Hash_Table{
+    int size;
+    Node *table;
+    int items;
+}
+
+int hash(char *word, Hash_Table *table);
+
+void rehash(Hash_Table *hash);
+
+Hash_Table* init();
+
+void insert(Hash_Table *hash char * key, Node val);
+
+Node get(Hash_Tabl *hash, int hash_value);
+
+double get_load_factor(Hash_Table *hash);
+
+Node popMax();
+
+int get_num_items(Hash_Table *hash);
+
+void remove();
