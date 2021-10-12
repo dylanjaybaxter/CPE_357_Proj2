4aa1c88565177c395d269b7971f7bfece1bc63b7
Dylan <dybaxter@calpoly.edu>
2021-10-11 20:17:38 -0700

    items to 0

--- hash.c
+++ hash.c
@@ -39,6 +39,7 @@ void rehash(Hash_Table *hash){
         hash->table[i].freq = 0;
     }
     hash->size = newSize;
+    hash->items = 0;
     for (i = 0; i < oldSize-1; i++){
         if (oldTable[i].word != NULL){
             insert(hash, oldTable[i].word, oldTable[i].freq);
